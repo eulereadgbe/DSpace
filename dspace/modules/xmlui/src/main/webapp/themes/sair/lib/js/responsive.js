5ec067a42e32a0c979f2724b8923631b7bdc476f
Elvi S. Nemiz <esnemiz@seafdec.org.ph>
2013-08-04 13:08:07 +0800

    Linkify All Items Metadata content (Item Detail View)

--- dspace/modules/xmlui/src/main/webapp/themes/sair/lib/js/responsive.js
+++ dspace/modules/xmlui/src/main/webapp/themes/sair/lib/js/responsive.js
@@ -36,9 +36,10 @@
         selector.click
         (function (event) {
             var elem = $(this).next();
+            var menu = $('#menu ul:visible');
             if (elem.is('ul')) {
                 event.preventDefault();
-                $('#menu ul:visible').not(elem).slideUp();
+                menu.not(elem).slideUp();
                 elem.slideToggle();
             }
         });
@@ -60,42 +61,50 @@
                 text = that.html();
             that.html(linkify_html(text));
         });
-    })(jQuery);
 
-    (function ($) {
-        $(document).ready(function () { // On load
-            $(window).resize(function () {
-                if ($(window).width() > 501) {
-                    $("#ds-header-wrapper").backstretch("/themes/sair/images/SAIR-banner.jpg");
-                }
-                if ($(window).width() <= 500) {
-                    $("#ds-header-wrapper").backstretch("/themes/sair/images/SAIR-banner-small.jpg");
-                }
+        //Open External Links In New Tab/Window
+        $('a').filter(function () {
+            return this.hostname && this.hostname !== location.hostname;
+        }).addClass("external");
+        $("#ds-body a[href^='http://']").attr("target", "_blank");
+    });
 
-                // Will not fire if screen widths is equal to values specified in media queries (media.css)
-                if ($(window).width() <= 972 && ($(window).width() !== 320 && $(window).width() !== 360 && $(window).width() !== 480 && $(window).width() !== 600)) {
-                    document.getElementById("ds-main").style.maxWidth = $(window).width() + 'px';
-                    document.getElementById("ds-main").style.width = (($(window).width() - 12) / $(window).width()) * 100 + '%';
-                    $("#ds-header-logo").backstretch("/themes/sair/images/seafdec-logo.png");
+})(jQuery);
 
-                    // This will update the width of breadcrumbs when resizing
-                    document.getElementById("breadCrumb").style.width = ($(window).width() - 12) + 'px';
-                    var nodes = document.getElementById("breadCrumb").childNodes;
-                    for (var i = 0; i < nodes.length; i++) {
-                        if (nodes[i].nodeName.toLowerCase() == 'div') {
-                            nodes[i].style.width = $(window).width() - 12 + 'px';
-                        }
+(function ($) {
+    $(document).ready(function () { // On load
+        $(window).resize(function () {
+            if ($(window).width() > 501) {
+                $("#ds-header-wrapper").backstretch("/themes/sair/images/SAIR-banner.jpg");
+            }
+            if ($(window).width() <= 500) {
+                $("#ds-header-wrapper").backstretch("/themes/sair/images/SAIR-banner-small.jpg");
+            }
+
+            // Will not fire if screen widths is equal to values specified in media queries (media.css)
+            if ($(window).width() <= 972 && ($(window).width() !== 320 && $(window).width() !== 360 && $(window).width() !== 480 && $(window).width() !== 600)) {
+                document.getElementById("ds-main").style.maxWidth = $(window).width() + 'px';
+                document.getElementById("ds-main").style.width = (($(window).width() - 12) / $(window).width()) * 100 + '%';
+                $("#ds-header-logo").backstretch("/themes/sair/images/seafdec-logo.png");
+
+                // This will update the width of breadcrumbs when resizing
+                document.getElementById("breadCrumb").style.width = ($(window).width() - 12) + 'px';
+                var nodes = document.getElementById("breadCrumb").childNodes;
+                for (var i = 0; i < nodes.length; i++) {
+                    if (nodes[i].nodeName.toLowerCase() == 'div') {
+                        nodes[i].style.width = $(window).width() - 12 + 'px';
                     }
                 }
-                if ($(window).width() <= 600) {
-                    $('div.ds-option-set ul.sublist').css('display', 'none');
-                }
-                else {
-                    $('div.ds-option-set ul.sublist').css('display', 'inherit');
-                }
-            });
-
-
-            $(window).resize();
+            }
+            if ($(window).width() <= 600) {
+                $('div.ds-option-set ul.sublist').css('display', 'none');
+            }
+            else {
+                $('div.ds-option-set ul.sublist').css('display', 'inherit');
+            }
         });
-    })(jQuery);
+
+
+        $(window).resize();
+    });
+})(jQuery);
