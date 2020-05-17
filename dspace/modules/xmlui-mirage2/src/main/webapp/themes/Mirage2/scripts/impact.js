f2422a1e02c18da480e9a3a4b57db442ff77a623
Elvi S. Nemiz <esnemiz@seafdec.org.ph>
2020-05-17 21:51:43 +0800

    Enabled altmetrics, show scopus citation counts

---
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/impact.js
@@ -0,0 +1,32 @@
+/**
+ * Functions to add citation count or other statistics from several
+ * services (like Scopus) to the impact section of an item view.
+ */
+
+var IMPACT_SERVLET_URL = "/impact/ImpactServlet.py";
+
+function addImpactServiceCitationCount(service) {
+    var idSelector = "#" + service;
+    var doi = $(idSelector).data("doi");
+    $.getJSON(IMPACT_SERVLET_URL + "?service=" + service + "&doi=" + doi)
+        .done(function(data) {
+            var citations = data["citationCount"];
+            var linkBack = data["linkBack"];
+            var record = data["linkBack"].replace('citedby.uri', 'record.uri')
+            if (citations && linkBack) {
+                $(idSelector + " a").attr("href", linkBack);
+                $(idSelector + " .citation-count").html(citations);
+                $(".scopus-record").attr("href", record);
+                $(idSelector).removeClass("hidden");
+            }
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            console.log("Error retrieving " + service + " citation count: " + error);
+        });
+}
+
+function addCitationCounts() {
+    addImpactServiceCitationCount("scopus");
+}
+
+$(document).ready(addCitationCounts);
